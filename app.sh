bcd4d3b8db4bf4c79a8a42eaadf8a1a578c268c8
educojr <48934713+educojr@users.noreply.github.com>
2019-03-26 22:31:29 -0300

    Add files via upload

---
+++ app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Functions
+}
+start(){
+  echo "Starting application"
+  cd /opt/yawoen_whoami/ && docker-compose up --build -d
+  [ $? -eq 0 ] && echo "Application started!!!" || echo "Error starting application"
+}
+
+stop(){
+  echo "Stopping aapli"
+  cd /opt/yawoen_whoami/ && docker-compose down
+  [ $? -eq 0 ] && echo "Application stopped!!!" || echo "Error stopping application"
+}
+
+# Main
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+esac
