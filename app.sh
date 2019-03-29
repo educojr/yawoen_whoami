60a8a3590ff69527b61bd25dd3c8be9bbe323574
educojr <48934713+educojr@users.noreply.github.com>
2019-03-29 17:59:35 -0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,14 +6,14 @@ hour=$(date +%H) # Check the hour
 # Functions
 start(){
   docker ps | grep whoami # Check if application is running
-  [ $? -eq 1 ] && echo "Starting application" || echo "Application is already running" && exit 0 # If it is not running, start application, else show that application is already running and exit
+  [ $? -eq 1 ] && echo "Starting application" || { echo "Application is already running" && exit 0; } # If it is not running, start application, else show that application is already running and exit
   cd /home/ubuntu/yawoen_whoami/ && docker-compose up --no-recreate -d # Go to application directory and start it
   [ $? -eq 0 ] && echo "Application started!!!" || echo "Error starting application" # Check if the application was started and print a little debug
 }
 
 stop(){
   docker ps | grep whoami # Check if application if running
-  [ $? -eq 0 ] && echo "Stopping aplication" || echo "Application is not running" && exit 0 # If it is running, stop application, else show that the application is not running and exit
+  [ $? -eq 0 ] && echo "Stopping aplication" || { echo "Application is not running" && exit 0; } # If it is running, stop application, else show that the application is not running and exit
   cd /home/ubuntu/yawoen_whoami/ && docker-compose down # Go to application directory and stop it
   [ $? -eq 0 ] && echo "Application stopped!!!" || echo "Error stopping application" # Check if the application was stopped and print a little debug
 }
